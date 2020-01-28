b9fa1c545bacd94589ec547aea5da6d04bd9111b
JC Smiley <jcsmileyjr@gmail.com>
2020-01-27 21:52:51 -0600

    Initial Nav bar setup and constant colors imported

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const colors = {
+    "headerColor":{"backgroundColor":"#06FE06"},
+    "textColor":{"color":"#17561D"},
+    "buttonColor":{"backgroundColor":"#69F969"}
+};
+
+export default colors;
