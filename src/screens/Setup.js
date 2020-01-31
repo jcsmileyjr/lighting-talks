43c2fa23ccb2e05fe25d67f8929793d51d518c7c
JC Smiley <jcsmileyjr@gmail.com>
2020-01-30 19:58:24 -0600

    Refactor and Restyle Setup screen based on wire-frame

--- src/screens/Setup.js
+++ src/screens/Setup.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../css/setup.css';
 
 import Header from '../components/header/Header';
 import Input from '../components/Input/Input';
@@ -8,21 +9,24 @@ import colors from '../constants';
 
 const Setup = props => {
     return(
-        <main style={colors.lightingBlue}>
+        <main >
             <Header title="Step 1/2" />
-            <Input  action={props.getSubject} 
-                    output={props.subject} 
-                    label="Subject" />
-            <TextArea   action={props.getDescription}
-                        output={props.description}
-                        label="Description" />
-            <Input  action={props.getSpeaker} 
-                    output={props.speaker}
-                    label="Speaker Name" />
-            <Input  action={props.getMinutes} 
-                    output = {props.minutes}
-                    label="Timer: Number of minutes" />
-            <PrimaryButton action={props.preview} />
+            <section>
+                <Input  action={props.getSubject} 
+                        output={props.subject} 
+                        label="Subject" />
+                <TextArea   action={props.getDescription}
+                            output={props.description}
+                            label="Description" />
+                <Input  action={props.getSpeaker} 
+                        output={props.speaker}
+                        label="Speaker Name" />
+                <Input  action={props.getMinutes} 
+                        output = {props.minutes}
+                        label="Timer: Number of minutes" />
+                <PrimaryButton action={props.preview} />
+            </section>
+
         </main>
     );
 }
