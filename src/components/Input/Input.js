6ddca3149d4a4fc9b15d8fe63561a02c040f5c6f
JC Smiley <jcsmileyjr@gmail.com>
2020-02-02 16:01:22 -0600

    Add icons to input fields

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,29 +1,36 @@
-import React from 'react';
-import './input.css';
+import React from "react";
+import "./input.css";
 
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBolt } from "@fortawesome/free-solid-svg-icons";
 
 const Input = props => {
+  const getValue = event => {
+    props.action(event.target.value);
+  };
 
-    const getValue = (event) => {
-        props.action(event.target.value);
-    }
+  return (
+    <Container fluid={true} className="containerStyle">
+      <Row>
+        <Col xs={0} sm={4} md={2} lg={4}></Col>
+        <Col xs={12} sm={4} md={8} lg={4}>
+          <input
+            onChange={getValue}
+            value={props.output}
+            type={props.type}
+            className="textStyle"
+            placeholder={props.label}
+          />
+          {props.output.length >= 1 &&
+            <i><FontAwesomeIcon icon={faBolt} /></i>          
+          }
 
-    return(
-        <Container fluid={true} className="containerStyle">
-            <Row>
-                <Col xs={0} sm={4} md={2} lg={4}></Col>
-                <Col xs={12} sm={4} md={8} lg={4}>
-                    <input  onChange={getValue} 
-                            value={props.output} 
-                            type={props.type} 
-                            className="textStyle"
-                            placeholder={props.label} />
-                </Col>
-                <Col xs={0} sm={4} md={2} lg={4}></Col>
-            </Row>
-        </Container>
-    );
-}
+        </Col>
+        <Col xs={0} sm={4} md={2} lg={4}></Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Input;
