1c2d48912e903302fc6f08ed43ea88a618554848
JC Smiley <jcsmileyjr@gmail.com>
2020-02-03 20:07:51 -0600

    Notes for the preview screen

--- src/screens/Preview.js
+++ src/screens/Preview.js
@@ -2,15 +2,17 @@ import React, {useState, useEffect} from 'react';
 import {Row, Col, Container, Button} from 'react-bootstrap';
 import '../css/preview.css';
 
-import PrimaryButton from '../components/Button/Button';
+import PrimaryButton from '../components/Button/Button';//Button to start the countdown timer
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
+//Screen to give the user a preview of the details inputted and then later start the countdown timer
 const Preview = props => { 
-    const [seconds, setSeconds] = useState(0);
-    const [min, setMin] = useState(props.minutes);
-    const [start, setStart] = useState(false);
+    const [seconds, setSeconds] = useState(0);//current number of seconds to be shown on the screen
+    const [min, setMin] = useState(props.minutes);//current number of minutes to be shown on the screen
+    const [start, setStart] = useState(false);//starts the countdown timer and show different elements on the screen (ex. back button)
 
+    //if the user press the start button, every second the timer will countdown
     useEffect(() => {
         let interval = null;
         if(start){
@@ -21,6 +23,7 @@ const Preview = props => {
         return () => clearInterval(interval);
     });
 
+    //timer function that update the seconds and min state that is shown in the UI
     let timer = () => {
         if(seconds <= 0){
             if(min <= 1){
@@ -35,12 +38,13 @@ const Preview = props => {
         }
     }
 
-    const begin = ()=> {setSeconds(60); setStart(true);};
-    const goBack = () => {props.back(2)}
+    const begin = ()=> {setSeconds(60); setStart(true);};//function to start the timer
+    const goBack = () => {props.back(2)}//function to go back to the Preview screen
 
     return(
         <main className="">
             <section className="contentStyle previewPageImage">
+                {/*The back button */}
                 {!start &&
                     <Container className="rightAlignIcon">
                         <Row>
@@ -59,7 +63,7 @@ const Preview = props => {
                 <h1 style={{"textDecoration":"underline"}}>{props.subject}</h1>
                 <h3>{props.description}</h3>
                 <p>By {props.speaker}</p>
-                
+                {/*If the start button is press, then the timer is shown and started */}
                 {
                     start?<p className="timer">{min-1} minutes : {seconds} seconds</p>:<h4>{props.minutes} minutes</h4>
                 }
